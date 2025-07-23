6ede83c9960bded32394ba80c86ed74638c11761
sagar <vyassagar4321@gmail.com>
2025-07-23 22:56:04 +0530

    basic start of Api Gateway service

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const {createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const PORT = 3004;
+
+app.use(morgan('combined'));
+app.use('/booking-service', createProxyMiddleware( {target: 'http://localhost:3002', changeOrigin: true} ));
+
+app.get('/home', (req, res) => {
+    return res.json({ message: 'Ok' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+});
